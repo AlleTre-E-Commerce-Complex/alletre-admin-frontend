eb561c81c1bd971a597830cd7274b98e460ea6ab
kamarudheen mv <kamaru916@gmail.com>
2024-08-27 09:20:57 +0530

    changed the version of react-drag-drop-files

--- src/config/axios-config.js
+++ src/config/axios-config.js
@@ -1,11 +1,12 @@
 import Axios from "axios";
 import auth from "../utils/auth";
 
-Axios.defaults.baseURL =
-  process.env.NODE_ENV !== "production"
-    ? process.env.REACT_APP_DEV_URL
-    : process.env.REACT_APP_SERVER_URL;
+// Axios.defaults.baseURL =
+//   process.env.NODE_ENV !== "production"  
+//     ? process.env.REACT_APP_DEV_URL
+//     : process.env.REACT_APP_SERVER_URL;
 
+Axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
 const AuthAxios = Axios.create();
 
 AuthAxios.interceptors.request.use(async (config) => {
